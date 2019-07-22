b5b3a77cfff6ed2d16aaf486a3ccc32acc7f3724
grif <grif@nanofo.com>
2019-07-22 16:55:17 +0300

    prepared convert_PascalCase_into_snake_case

---
+++ convert_PascalCase_into_snake_case.js
@@ -0,0 +1,10 @@
+function main(str) {
+    return str;
+}
+
+function example(str) {
+    return str;
+}
+
+module.exports.main = main;
+module.exports.example = example;
--- handler/handler.js
+++ handler/handler.js
@@ -3,8 +3,10 @@ const firstNonRepeatingLetter = require('../first_non_repeating_letter');
 const tortoiseRacing = require('../tortoiseRacing');
 const greedIsGood = require('../greedIsGood');
 const isMyFriendCheating = require('../isMyFriendCheating');
+const convertPascalCase = require('../convert_PascalCase_into_snake_case');
 
 // exec.execute(firstNonRepeatingLetter, 'firstNonRepeatingLetter', 'moonmen');
 // exec.execute(tortoiseRacing, 'tortoiseRacing', {v1: 720, v2: 850, g: 70});
 // exec.execute(greedIsGood, 'greedIsGood', [2, 4, 4, 5, 4]);
-exec.execute(isMyFriendCheating, 'isMyFriendCheating', 1000008, 1000);
+// exec.execute(isMyFriendCheating, 'isMyFriendCheating', 1000008, 1000);
+exec.execute(convertPascalCase, 'convertPascalCase', 'TestController', 1000);
