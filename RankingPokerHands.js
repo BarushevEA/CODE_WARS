01d256a04ef3ac43c603a2ee84ce201348c86c90
grif <grif@nanofo.com>
2019-07-24 16:38:36 +0300

    prepare RankingPokerHands

---
+++ RankingPokerHands.js
@@ -0,0 +1,10 @@
+function main(string) {
+    return '1';
+}
+
+function example(string) {
+    return '2';
+}
+
+module.exports.main = main;
+module.exports.example = example;
--- handler/handler.js
+++ handler/handler.js
@@ -3,10 +3,11 @@ const firstNonRepeatingLetter = require('../first_non_repeating_letter');
 const tortoiseRacing = require('../tortoiseRacing');
 const greedIsGood = require('../greedIsGood');
 const isMyFriendCheating = require('../isMyFriendCheating');
-const convertPascalCase = require('../convert_PascalCase_into_snake_case');
+const RankingPokerHands = require('../RankingPokerHands');
 
 // exec.execute(firstNonRepeatingLetter, 'firstNonRepeatingLetter', 'moonmen');
 // exec.execute(tortoiseRacing, 'tortoiseRacing', {v1: 720, v2: 850, g: 70});
 // exec.execute(greedIsGood, 'greedIsGood', [2, 4, 4, 5, 4]);
 // exec.execute(isMyFriendCheating, 'isMyFriendCheating', 1000008, 1000);
-exec.execute(convertPascalCase, 'convertPascalCase', 'convertPascalCase', 1000000);
+// exec.execute(convertPascalCase, 'convertPascalCase', 'convertPascalCase', 1000000);
+exec.execute(RankingPokerHands, 'RankingPokerHands', 'convertPascalCase', 1000000);
